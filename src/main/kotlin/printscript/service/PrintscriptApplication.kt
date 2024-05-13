89638433ac955a9cbc655fa7421ce3d392a186bb
juancruzestevez02 <juan.estevez@ing.austral.edu.ar>
2024-05-13 15:10:09 -0300

    inicializacion

--- src/main/kotlin/printscript/service/PrintscriptApplication.kt
+++ src/main/kotlin/printscript/service/PrintscriptApplication.kt
@@ -1,10 +1,17 @@
 package printscript.service
 
+import io.github.cdimascio.dotenv.Dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class PrintscriptApplication
+class PrintscriptApplication{
+	@Bean
+	fun dotenv(): Dotenv {
+		return Dotenv.configure().load()
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<PrintscriptApplication>(*args )
