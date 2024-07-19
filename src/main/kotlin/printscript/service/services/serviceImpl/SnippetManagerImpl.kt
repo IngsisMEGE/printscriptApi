2d73ae78022bf9c3313e52ae20192d3cf06f9e0f
Gonza69420 <gonzalo.mato@ing.austral.edu.ar>
2024-07-19 07:48:23 -0300

    - changed object mapper for snippet status

--- src/main/kotlin/printscript/service/services/serviceImpl/SnippetManagerImpl.kt
+++ src/main/kotlin/printscript/service/services/serviceImpl/SnippetManagerImpl.kt
@@ -1,17 +1,23 @@
 package printscript.service.services.serviceImpl
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Service
 import printscript.service.dto.StatusDTO
 import printscript.service.services.interfaces.SnippetManagerService
 
 @Service
-class SnippetManagerImpl(private val redisTemplate: RedisTemplate<String, Any>) : SnippetManagerService {
-    private val objectMapper = jacksonObjectMapper()
+class SnippetManagerImpl(
+    private val redisTemplate: RedisTemplate<String, Any>,
+    private val objectMapper: ObjectMapper,
+) : SnippetManagerService {
+    private val logger: Logger = LoggerFactory.getLogger(SnippetManagerImpl::class.java)
 
     override fun updateSnippetStatus(newStatus: StatusDTO) {
         val requestData = objectMapper.writeValueAsString(newStatus)
         redisTemplate.opsForList().rightPush("snippet_sca_status", requestData)
+        logger.debug("Queued snippet status update request for snippetId: ${newStatus.id}")
     }
 }
